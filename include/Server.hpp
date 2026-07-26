7a8b8927035cde20ca0f843c4aa306b5fea48b31
Hasan Chowdhuri <hchowdhu@k2r4p1.42mulhouse.fr>
2026-07-27 00:06:23 +0200

    Added main entry point with argument validation and error handling

--- include/Server.hpp
+++ include/Server.hpp
@@ -6,7 +6,62 @@
 /*   By: hchowdhu <hchowdhu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/13 00:11:21 by hchowdhu          #+#    #+#             */
-/*   Updated: 2026/07/13 00:11:22 by hchowdhu         ###   ########.fr       */
+/*   Updated: 2026/07/27 00:03:51 by hchowdhu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SERVER_HPP
+# define SERVER_HPP
+
+# include "Client.hpp"
+# include <map>
+# include <poll.h>
+# include <string>
+# include <vector>
+
+class Server
+{
+	private:
+		int					_port;
+		std::string			_password;
+		int					_serverFd;
+		bool				_running;
+		std::vector<struct pollfd>	_pollFds;
+		std::map<int, Client>		_clients;
+
+		Server();
+		Server(const Server &other);
+		Server &operator=(const Server &other);
+
+		int		parsePort(const std::string &port) const;
+		void	createListeningSocket();
+		void	setNonBlocking(int fd) const;
+		void	addPollFd(int fd, short events);
+		void	removePollFd(int fd);
+		void	setPollEvents(int fd, short events);
+
+		void	acceptClients();
+		bool	receiveFromClient(int fd);
+		bool	flushClientOutput(int fd);
+		bool	processClientBuffer(Client &client);
+		bool	processLine(Client &client, const std::string &line);
+
+	public:
+		Server(const std::string &port, const std::string &password);
+		~Server();
+
+		void	init();
+		void	run();
+		void	stop();
+
+		int					getPort() const;
+		const std::string	&getPassword() const;
+		Client				*getClient(int fd);
+		const Client		*getClient(int fd) const;
+		std::map<int, Client>	&getClients();
+
+		void	queueMessage(int fd, const std::string &message);
+		void	disconnectClient(int fd, const std::string &reason);
+};
+
+#endif
--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -6,7 +6,105 @@
 /*   By: hchowdhu <hchowdhu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/13 00:12:27 by hchowdhu          #+#    #+#             */
-/*   Updated: 2026/07/13 00:12:29 by hchowdhu         ###   ########.fr       */
+/*   Updated: 2026/07/27 00:03:00 by hchowdhu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Server.hpp"
+#include <cctype>
+#include <iostream>
+#include <stdexcept>
+#include <unistd.h>
+
+Server::Server(const std::string &port, const std::string &password)
+	: _port(parsePort(port)), _password(password), _serverFd(-1),
+	  _running(false), _pollFds(), _clients()
+{
+	if (_password.empty())
+		throw (std::runtime_error("Password cannot be empty"));
+}
+
+Server::~Server()
+{
+	for (std::vector<struct pollfd>::iterator it = _pollFds.begin();
+		it != _pollFds.end(); ++it)
+	{
+		if (it->fd >= 0)
+			close(it->fd);
+	}
+	_pollFds.clear();
+	_clients.clear();
+	_serverFd = -1;
+}
+
+int Server::getPort() const
+{
+	return (_port);
+}
+
+const std::string &Server::getPassword() const
+{
+	return (_password);
+}
+
+Client *Server::getClient(int fd)
+{
+	std::map<int, Client>::iterator it = _clients.find(fd);
+
+	if (it == _clients.end())
+		return (NULL);
+	return (&it->second);
+}
+
+const Client *Server::getClient(int fd) const
+{
+	std::map<int, Client>::const_iterator it = _clients.find(fd);
+
+	if (it == _clients.end())
+		return (NULL);
+	return (&it->second);
+}
+
+std::map<int, Client> &Server::getClients()
+{
+	return (_clients);
+}
+
+void Server::queueMessage(int fd, const std::string &message)
+{
+	Client *client = getClient(fd);
+
+	if (client == NULL)
+		return ;
+	client->outputBuffer() += message;
+	if (message.size() < 2
+		|| message.substr(message.size() - 2) != "\r\n")
+		client->outputBuffer() += "\r\n";
+	setPollEvents(fd, POLLIN | POLLOUT);
+}
+
+bool Server::processLine(Client &client, const std::string &line)
+{
+	std::string command;
+	size_t position = line.find(' ');
+
+	if (position == std::string::npos)
+		command = line;
+	else
+		command = line.substr(0, position);
+	for (size_t i = 0; i < command.size(); ++i)
+		command[i] = static_cast<char>(std::toupper(
+			static_cast<unsigned char>(command[i])));
+	std::cout << "[client " << client.getFd() << "] " << line << std::endl;
+	if (command == "PING")
+	{
+		if (position == std::string::npos || position + 1 >= line.size())
+			queueMessage(client.getFd(), ":irc.local 409 * :No origin specified");
+		else
+			queueMessage(client.getFd(), "PONG " + line.substr(position + 1));
+	}
+	else if (command == "QUIT")
+		return (false);
+	/* Replace this function body with Parser/Command dispatch later. */
+	return (true);
+}
